0cb653f630c4ca336fef777c69e1b1ee26560d66
Edwin Njeru <mailnjeru@gmail.com>
2018-06-04 10:45:38 +0300

    Reformatted code to adhere to checkstyle conditions

--- src/main/java/io/github/scalified/tree/TraversalAction.java
+++ src/main/java/io/github/scalified/tree/TraversalAction.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2018 Edwin Njeru (mailnjeru@gmail.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,19 +25,19 @@ package io.github.scalified.tree;
  */
 public interface TraversalAction<T extends TreeNode> {
 
-	/**
-	 * Is called on each node, while traversing the tree
-	 *
-	 * @param node reference to the current node during tree traversal
-	 */
-	void perform(T node);
+    /**
+     * Is called on each node, while traversing the tree
+     *
+     * @param node reference to the current node during tree traversal
+     */
+    void perform(T node);
 
-	/**
-	 * Checks whether the traversal is completed and no more required
-	 *
-	 * @return {@code true} if traversal is completed and no more required,
-	 *         {@code false} otherwise
-	 */
-	boolean isCompleted();
+    /**
+     * Checks whether the traversal is completed and no more required
+     *
+     * @return {@code true} if traversal is completed and no more required,
+     *         {@code false} otherwise
+     */
+    boolean isCompleted();
 
 }
--- src/main/java/io/github/scalified/tree/TreeNodeException.java
+++ src/main/java/io/github/scalified/tree/TreeNodeException.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2018 Edwin Njeru (mailnjeru@gmail.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,28 +25,28 @@ package io.github.scalified.tree;
  */
 public class TreeNodeException extends RuntimeException {
 
-	/**
-	 * Constructs a new tree node exception with the specified detail message
-	 *
-	 * @param message the detail message. The detail message is saved for
-	 *                later retrieval by the {@link #getMessage()} method
-	 */
-	public TreeNodeException(String message) {
-		super(message);
-	}
+    /**
+     * Constructs a new tree node exception with the specified detail message
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method
+     */
+    public TreeNodeException(String message) {
+        super(message);
+    }
 
-	/**
-	 * Constructs a new tree node exception with the specified detail message and cause
-	 *
-	 * @param message the detail message. The detail message is saved for
-	 *                later retrieval by the {@link #getMessage()} method
-	 * @param  cause the cause (which is saved for later retrieval by the
-	 *               {@link #getCause()} method). A {@code null} value is
-	 *               permitted, and indicates that the cause is nonexistent
-	 *               or unknown
-	 */
-	public TreeNodeException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    /**
+     * Constructs a new tree node exception with the specified detail message and cause
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method
+     * @param  cause the cause (which is saved for later retrieval by the
+     *               {@link #getCause()} method). A {@code null} value is
+     *               permitted, and indicates that the cause is nonexistent
+     *               or unknown
+     */
+    public TreeNodeException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
 }
